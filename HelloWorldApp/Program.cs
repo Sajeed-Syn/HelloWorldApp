c65e00fc015a5aa7319496f0660c76e5129ce87a
sajeedrs <sajeedshaikh@synergetics-india.com>
2022-07-12 17:50:09 +0530

    modified

--- HelloWorldApp/Program.cs
+++ HelloWorldApp/Program.cs
@@ -7,7 +7,7 @@ namespace HelloWorldApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("This is line 1");
+            Console.WriteLine("This is line 2");
             Console.WriteLine("Welcome to Net");
         }
     }
